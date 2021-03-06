59e1a97b6075f95e03732ec9bf3584c97a7ba835
Metehan Gulac <metehanglc@protonmail.com>
2021-03-07 00:16:01 +0300

    UPDATE: new theme conf

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 import parasut_cli
+import sphinx_rtd_theme
 
 # -- General configuration ---------------------------------------------
 
@@ -31,7 +32,11 @@ import parasut_cli
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
