666936a82090a992f86b1e5934e9f8e40cf112bf
Metehan Gulac <metehanglc@protonmail.com>
2021-03-06 23:17:39 +0300

    UPDATE: docs config update

--- docs/conf.py
+++ docs/conf.py
@@ -31,11 +31,7 @@ import parasut_cli
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx_rtd_theme',
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -99,7 +95,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_theme_path = ["_themes", ]
 
 
 # -- Options for HTMLHelp output ---------------------------------------
