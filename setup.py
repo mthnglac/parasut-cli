c5c5ad8a325ae92188db9ea7b5b64a57ba5ccde1
Metehan Gulac <metehanglc@protonmail.com>
2021-08-13 16:03:40 +0300

    Bump version: 0.4.4 → 0.4.5

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mthnglac/parasut-cli',
-    version='0.4.4',
+    version='0.4.5',
     zip_safe=False,
 )
