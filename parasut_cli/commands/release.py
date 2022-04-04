695d84b5be5b21ce53911b5c5caf31fe5ecbc642
Metehan Gulac <metehanglc@protonmail.com>
2022-04-04 11:35:10 +0300

    pre-release commented. because it is still not tested.

--- parasut_cli/commands/release.py
+++ parasut_cli/commands/release.py
@@ -9,27 +9,33 @@ class ReleaseCommand(Command):
         self,
         receiver: Receiver,
         target_repo: str,
-        pre_release: bool,
+        # pre_release: bool,
         auto_login: bool,
         show_output: bool,
     ) -> None:
         self._receiver: Receiver = receiver
         self._target_repo: str = target_repo
-        self._pre_release: bool = pre_release
+        # self._pre_release: bool = pre_release
         self._auto_login: bool = auto_login
         self._show_output: bool = show_output
 
     def execute(self) -> None:
-        if self._target_repo and not self._pre_release:
-            self._receiver.release_repo(
-                target_repo=self._target_repo,
-                show_output=self._show_output,
-                auto_login=self._auto_login,
-            )
+        self._receiver.release_repo(
+            target_repo=self._target_repo,
+            show_output=self._show_output,
+            auto_login=self._auto_login,
+        )
 
-        if self._target_repo and self._pre_release:
-            self._receiver.pre_release_repo(
-                target_repo=self._target_repo,
-                show_output=self._show_output,
-                auto_login=self._auto_login,
-            )
+        # if self._target_repo and not self._pre_release:
+            # self._receiver.release_repo(
+                # target_repo=self._target_repo,
+                # show_output=self._show_output,
+                # auto_login=self._auto_login,
+            # )
+
+        # if self._target_repo and self._pre_release:
+            # self._receiver.pre_release_repo(
+                # target_repo=self._target_repo,
+                # show_output=self._show_output,
+                # auto_login=self._auto_login,
+            # )
