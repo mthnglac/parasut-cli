1d89242e62b0f56a62466bc3f8e205d55f55ddd7
Metehan Gulac <metehanglc@protonmail.com>
2022-04-11 10:17:21 +0300

    fix: forgotten class variables

--- parasut_cli/utils/receiver.py
+++ parasut_cli/utils/receiver.py
@@ -106,8 +106,6 @@ class Receiver:
             source_asdf="source ~/.asdf/asdf.sh",
             ember_release="ember release",
             ember_release_all_yes="ember release --yes",
-            ember_pre_release=f"ember release -l --prerelease {self._get_valid_pre_release_name()}",
-            ember_pre_release_all_yes=f"ember release -l --prerelease {self._get_valid_pre_release_name()} --yes",
             git_change_branch_master="git checkout master",
             git_change_branch_develop="git checkout develop",
             git_fetch_all="git fetch --all",
